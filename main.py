dd9b07c79b4d5e85300cc4fe36cea89cf3c6e2bc
dharalnaik <142478246+dharalnaik@users.noreply.github.com>
2024-03-31 16:46:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -203,19 +203,3 @@ async def delete_product(product_name: str, db:db_dependency):
     return result
 
 #------------------------
-    
-
-'''
-@app.get("/questions/{question_id}")
-async def read_question(question_id: int, db: db_dependency):
-    result = db.query(models.Questions).filter(models.Questions.id == question_id).first()
-    if not result:
-        raise HTTPException(status_code = 404, detail='Question is not found')
-    return result
-
-@app.get("/choices/{question_id}")
-async def read_choices(question_id: int, db: db_dependency):
-    result = db.query(models.Choices).filter(models.Choices.question_id == question_id).all()
-    if not result:
-        raise HTTPException(status_code = 404, detail='Choices are not found')
-    return result'''
